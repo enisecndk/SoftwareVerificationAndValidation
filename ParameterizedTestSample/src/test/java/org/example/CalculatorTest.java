c852f78cec4405e8dda08c83b23e41e1dc0d562c
enisecndk <enisecandik@gmail.com>
2023-05-26 23:16:53 +0300

    Homework

---
+++ ParameterizedTestSample/src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,47 @@
+// SOFTWARE VERIFICATION AND VALIDATION HOMEWORK
+// Student id: B211202404
+// Student name surname: Enise Nur ÇANDİK
+// Repository address:
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+    @AfterEach
+    void tearDown() {
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "10, 2, 5",
+            "10, 4, 2.5",
+            "12.5, 5, 2.5",
+            "10, 2.5, 4",
+            "12.5, 2.5, 5"
+    })
+
+        //EQUALITY
+    void testDivision1(double a, double b, double c) {
+        assertEquals(c, Calculator.divide(a, b));
+    }
+
+        //EXCEPTION
+    @Test
+    void testDivision2() {
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> Calculator.divide(12.5, 0),
+                "IllegalArgumentException expected."
+        );
+
+    }
+}
